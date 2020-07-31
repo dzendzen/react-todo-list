841f320425d551f691913f6931011604901ec77f
dzendzen <dzen@hotmail.fr>
2020-07-31 10:32:06 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import uuid from "uuid";
+// import uuid from "uuid";
 import TodoInput from "./Components/TodoInput";
 import TodoList from "./Components/TodoList";
 
